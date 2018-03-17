57664b38758f6ef02c24ce9d467327a68a7f2f6d
Ecksi <jimmy.dechant@gmail.com>
2018-03-17 11:42:53 -0600

    Add initial Platform tests

---
+++ test/Platform-test.js
@@ -0,0 +1,29 @@
+const { assert } = require('chai');
+const Platform = require('../lib/Platform.js')
+
+describe('Platform', function () {
+  let platform;
+
+  beforeEach(function () {
+    platform = new Platform();
+  })
+
+  it('should exist', () => {
+    assert.equal(true, true);
+  })
+
+  it('Platform class should be a function', () => {
+    assert.isFunction(Platform);
+  })
+
+  it('a new platform should be an object', () => {
+    assert.isObject(platform);
+  })
+
+  it('should draw a platform when instantiated', () => {
+    assert.equal(platform.platforms[0].x, 60);
+    assert.equal(platform.platforms[0].y, 580);
+    assert.equal(platform.platforms[0].w, 870);
+    assert.equal(platform.platforms[0].h, 10);
+  })
+});
