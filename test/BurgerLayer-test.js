0e5cbdd394c05670d5a5524a5676a7b1891691aa
Ecksi <jimmy.dechant@gmail.com>
2018-03-23 21:23:16 -0600

    Fix broken tests for BurgerLayer-test.js

--- test/BurgerLayer-test.js
+++ test/BurgerLayer-test.js
@@ -1,63 +1,64 @@
 const { assert } = require('chai');
-const BurgerLayer = require('../lib/BurgerLayer.js')
+const Burger = require('../lib/BurgerLayer.js')
 
-describe('BurgerLayer', () => {
-  let burgerLayer;
+describe('Burger', () => {
+  let burger1;
+  let burger2;
+  let burger3;
+  let burger4;
 
   beforeEach(() => {
-    burgerLayer = new BurgerLayer();
+    burger1 = new Burger(55, 75, 'top');
+    burger2 = new Burger(350, 280, 'lettuce');
+    burger3 = new Burger(525, 130, 'patty');
+    burger4 = new Burger(780, 420, 'bottom');
   })
 
-  it('BurgerLayer class should be a function', () => {
-    assert.isFunction(BurgerLayer);
+  it('Burger class should be a function', () => {
+    assert.isFunction(Burger);
   })
 
-  it('a new burgerLayer should be an object', () => {
-    assert.isObject(burgerLayer);
+  it('a new burger should be an object', () => {
+    assert.isObject(burger1);
   })
 
-  it('should draw a burgerLayer when instantiated', () => {
-    assert.equal(burgerLayer.burgerLayers[0].x, 125);
-    assert.equal(burgerLayer.burgerLayers[0].y, 195);
+  it('should have the same x coord as the x argument', () => {
+    assert.equal(burger1.x, 55)
   })
 
   it('should have a width of 125px', () => {
-    assert.equal(burgerLayer.burgerLayers[0].w, 125);
+    assert.equal(burger2.w, 125);
   })
 
   it('should have a height of 15px', () => {
-    assert.equal(burgerLayer.burgerLayers[0].h, 15);
-  })
-
-  it('should start with 16 burgerLayers', () => {
-    assert.equal(burgerLayer.burgerLayers.length, 16)
+    assert.equal(burger3.h, 15);
   })
 
   it('should start with a smushLeft count of 0', () => {
-    assert.equal(burgerLayer.burgerLayers[8].smushLeft, 0)
+    assert.equal(burger2.smushLeft, 0)
   })
 
   it('should start with a smushRight count of 0', () => {
-    assert.equal(burgerLayer.burgerLayers[4].smushRight, 0)
+    assert.equal(burger3.smushRight, 0)
   })
 
   it('should start with a smushCount count of 0', () => {
-    assert.equal(burgerLayer.burgerLayers[15].smushCount, 0)
+    assert.equal(burger4.smushCount, 0)
   })
 
-  it('should be able to have a layer type of top', () => {
-    assert.equal(burgerLayer.burgerLayers[1].layer, 'top')
+  it('should be able to have a  type of top', () => {
+    assert.equal(burger1.layer, 'top')
   })
 
-  it('should be able to have a layer type of lettuce', () => {
-    assert.equal(burgerLayer.burgerLayers[5].layer, 'lettuce')
+  it('should be able to have a  type of lettuce', () => {
+    assert.equal(burger2.layer, 'lettuce')
   })
 
-  it('should be able to have a layer type of patty', () => {
-    assert.equal(burgerLayer.burgerLayers[9].layer, 'patty')
+  it('should be able to have a  type of patty', () => {
+    assert.equal(burger3.layer, 'patty')
   })
 
-  it('should be able to have a layer type of bottom', () => {
-    assert.equal(burgerLayer.burgerLayers[14].layer, 'bottom')
+  it('should be able to have a  type of bottom', () => {
+    assert.equal(burger4.layer, 'bottom')
   })
 });
