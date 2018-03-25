7b295b6af42d82b4f03da6479e7a5ecb68c515d5
Ecksi <jimmy.dechant@gmail.com>
2018-03-25 08:35:25 -0600

    Move level1 creation tests from game-test to level1-test

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,8 +1,6 @@
 const { assert } = require('chai');
 const Game = require('../lib/Game');
 
-require('locus');
-
 describe('Game', () => {
   let game;
 
@@ -62,38 +60,5 @@ describe('Game', () => {
 
   it('should be able to create a new text object', () => {
     assert.equal(typeof game.text, 'object');
-  })
-
-  it('should be able to create the burgers for Level 1', () => {
-    assert.equal(game.burgerLayers[2].x, 535);
-    assert.equal(game.burgerLayers[2].y, 110);
-    assert.equal(game.burgerLayers[3].w, 125);
-    assert.equal(game.burgerLayers[4].h, 15);
-    assert.equal(game.burgerLayers[1].layer, 'top');
-    assert.equal(game.burgerLayers.length, 16);
-  })
-
-  it('should be able to create the ladders for Level 1', () => {
-    assert.equal(game.ladders[1].x, 65);
-    assert.equal(game.ladders[3].y, 325);
-    assert.equal(game.ladders[3].w, 30);
-    assert.equal(game.ladders[4].h, 100);
-    assert.equal(game.ladders.length, 31);
-  })
-
-  it('should be able to create the platforms for Level 1', () => {
-    assert.equal(game.platforms[6].x, 265);
-    assert.equal(game.platforms[4].y, 315);
-    assert.equal(game.platforms[2].w, 490);
-    assert.equal(game.platforms[8].h, 10);
-    assert.equal(game.platforms.length, 10);
-  })
-
-  it('should be able to create the plates for Level 1', () => {
-    assert.equal(game.platesOnBottom[1].x, 330);
-    assert.equal(game.platesOnBottom[3].y, 675);
-    assert.equal(game.platesOnBottom[2].w, 135);
-    assert.equal(game.platesOnBottom[0].h, 5);
-    assert.equal(game.platesOnBottom.length, 4);
   })
 })
--- test/Level1-test.js
+++ test/Level1-test.js
@@ -1,2 +1,40 @@
 const { assert } = require('chai');
-const Level1 = require('../lib/Level1');
+const level1 = require('../lib/Level1');
+
+require('locus');
+
+
+describe('level1', () => {
+  it('should be able to create the burgers for Level 1', () => {
+    assert.equal(level1[0].burgers[2].x, 535);
+    assert.equal(level1[0].burgers[2].y, 110);
+    assert.equal(level1[0].burgers[3].w, 125);
+    assert.equal(level1[0].burgers[4].h, 15);
+    assert.equal(level1[0].burgers[1].layer, 'top');
+    assert.equal(level1[0].burgers.length, 16);
+  })
+
+  it('should be able to create the ladders for Level 1', () => {
+    assert.equal(level1[1].ladders[1].x, 65);
+    assert.equal(level1[1].ladders[3].y, 325);
+    assert.equal(level1[1].ladders[3].w, 30);
+    assert.equal(level1[1].ladders[4].h, 100);
+    assert.equal(level1[1].ladders.length, 31);
+  })
+
+  it('should be able to create the platforms for Level 1', () => {
+    assert.equal(level1[2].platforms[6].x, 265);
+    assert.equal(level1[2].platforms[4].y, 315);
+    assert.equal(level1[2].platforms[2].w, 490);
+    assert.equal(level1[2].platforms[8].h, 10);
+    assert.equal(level1[2].platforms.length, 10);
+  })
+
+  it('should be able to create the plates for Level 1', () => {
+    assert.equal(level1[3].plates[1].x, 330);
+    assert.equal(level1[3].plates[3].y, 675);
+    assert.equal(level1[3].plates[2].w, 135);
+    assert.equal(level1[3].plates[0].h, 5);
+    assert.equal(level1[3].plates.length, 4);
+  })
+})
