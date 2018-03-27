b11d6b8e7973419dba5d844f866cd98614d90cdd
Ecksi <jimmy.dechant@gmail.com>
2018-03-27 15:35:00 -0600

    Add findCurrentBurger test

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,6 +1,5 @@
 const { assert } = require('chai');
 const Game = require('../lib/Game');
-const Platform = require('../lib/Platform');
 
 require('locus');
 
@@ -24,19 +23,19 @@ describe('Game', () => {
 
   it('Game class should be a function', () => {
     assert.isFunction(Game);
-  })
+  });
 
   it('should be an object', () => {
     assert.isObject(game);
-  })
+  });
 
   it('should start with a score of 0', () => {
     assert.equal(game.score, 0);
-  })
+  });
 
   it('should start with a timer set at 31', () => {
     assert.equal(game.timer, 51);
-  })
+  });
 
   it('should decrement the counter as the game goes on', () => {
     game.gameTimer();
@@ -46,35 +45,35 @@ describe('Game', () => {
     game.gameTimer();
 
     assert.equal(game.timer, 50.925);
-  })
+  });
 
   it('should start with a gameNumber of 1', () => {
     assert.equal(game.gameNumber, 1);
-  })
+  });
 
   it('should be able to create a new hero object', () => {
     assert.equal(typeof game.hero, 'object');
-  })
+  });
 
   it('should be able to create a new ladder object', () => {
     assert.equal(typeof game.ladder, 'object');
-  })
+  });
 
   it('should be able to create a new platform object', () => {
     assert.equal(typeof game.platform, 'object');
-  })
+  });
 
   it('should be able to create a new burgerLayer object', () => {
     assert.equal(typeof game.burgerLayers, 'object');
-  })
+  });
 
   it('should be able to create a new plate object', () => {
     assert.equal(typeof game.plates, 'object');
-  })
+  });
 
   it('should be able to create a new text object', () => {
     assert.equal(typeof game.text, 'object');
-  })
+  });
 
   it('should be able to the move to the right', () => {
     game = new Game(undefined, undefined, keyboarder);
@@ -85,7 +84,7 @@ describe('Game', () => {
     game.moveHero();
 
     assert.equal(game.hero.x, originalHeroXPosition + 2.5)
-  })
+  });
 
   it('should be able to the move to the left', () => {
     game = new Game(undefined, undefined, keyboarder);
@@ -96,7 +95,7 @@ describe('Game', () => {
     game.moveHero();
 
     assert.equal(game.hero.x, originalHeroXPosition - 2.5)
-  })
+  });
 
   it('should be able to the move to the down', () => {
     game = new Game(undefined, undefined, keyboarder);
@@ -107,7 +106,7 @@ describe('Game', () => {
     game.moveHero();
 
     assert.equal(game.hero.y, originalHeroYPosition + 2.5)
-  })
+  });
 
   it('should be able to the move to the up', () => {
     game = new Game(undefined, undefined, keyboarder);
@@ -118,7 +117,7 @@ describe('Game', () => {
     game.moveHero();
 
     assert.equal(game.hero.y, originalHeroYPosition - 2.5)
-  })
+  });
 
   it('should be able to tell if the hero is on a platform', () => {
     assert.equal(game.hero.onPlatform, false);
@@ -126,7 +125,7 @@ describe('Game', () => {
     game.platformCollision();
 
     assert.equal(game.hero.onPlatform, true);
-  })
+  });
 
   it('should be able to tell if the hero is on a ladder', () => {
     game = new Game(undefined, undefined, keyboarder);
@@ -145,5 +144,14 @@ describe('Game', () => {
     game.ladderCollision();
 
     assert.equal(game.hero.onLadder, false);
-  })
+  });
+
+  it('should be able to tell what burger the hero is on', () => {
+    game = new Game(undefined, undefined, keyboarder);
+    
+    game.hero.x = 600;
+    game.hero.y = 150;
+
+    assert.equal(game.findCurrentBurger().length, 1)
+  });
 })
